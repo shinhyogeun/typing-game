481132ce1da5ebe6f06250d246a718b93fc93d14
shinhyogeun <tls1gy2rms3@naver.com>
2020-07-06 14:33:42 +0900

    7/6 문장 애니메이션 효과

--- Project_Tadak/Brain/Calculate.swift
+++ Project_Tadak/Brain/Calculate.swift
@@ -7,45 +7,79 @@
 //
 
 import Foundation
+import UIKit
 
-struct Calculate {
+class Calculate {
     
     var miss = 0
+    var aa = 2
     
-    mutating func calculateHowManyMissYouMake(_ original : String,  whatYouAreWriting: String) -> Int{
-        
-        //오타의 종류에는 3가지가 있다.
-        //1. 쓸 것을 아무것도 쓰지 않았을 때 => 배열의 길이가 달라져서 쓰지 않은 부분뒤로 모두 틀리게 된다. => 아무것도 쓰지않은 곳이 발각되면 우리가 무엇을 넣어서 계속 나아갈까?
-        //2. 쓸 것에 엉뚱한 것을 쓸 때 => 이것은 배열의 길이가 똑같이 때문에 우리가 오타의 갯수를 찾기가 쉽다.
-        //3. 안 써야 할 곳에 무엇을 쓸 때 => 이것도 배열의 길이가 달려져서 더 쓴 부분뒤로 모두 틀리게 된다. => 무엇을 더 쓴 곳이 있으면 그것을 지울까..?
-        // 안녕하세요 =  ["ㅇ","ㅏ","ㄴ","ㄴ","ㅕ"]
-        //  녕하세요 = [" ","ㄴ","ㅕ"]
-        let jamoYouAreWriting2 = Jamo.getJamo(whatYouAreWriting)
-        let jamoYouHaveToWriting2 = Jamo.getJamo(original)
-        
-        let originalJamoArray = Array(jamoYouHaveToWriting2)
-        let jamoYouAreWriting = Array(jamoYouAreWriting2)
-        
-        miss = 0
-        
-        for number in 0..<jamoYouAreWriting.count{
-            if jamoYouAreWriting[number] != originalJamoArray[number]{
-                if jamoYouAreWriting[number] == " "{
-//                    여기는 큰 실수가 일어난 구간이지. 잘못으로 스페이스바를 해서 한 글자를 통채로 안씀..
-                }
-//                print(jamoYouAreWriting[number])
-//                print(originalJamoArray[number])
-                self.miss = miss + 1
-            }
-        }
-        
-        print(miss)
-        
+     func countMiss(_ input : String, whatYouHaveToWrite : String, whatYouAlreadyWrite : String) -> Int {
+        if Array(input).count < aa-1 {
+                            if Array(input).count == 0{
+                                self.aa = 2
+                            }else{
+                                self.aa = Array(input).count + 1
+                            }
+                        } else if Array(input).count == aa {
+                                
+                                var compare1 = Array(Jamo.getJamo(String(Array(whatYouAlreadyWrite)[aa-2]))).count
+                                var compare2 = Array(Jamo.getJamo(String(Array(whatYouHaveToWrite)[aa-2]))).count
+                            
+                                if compare2 > compare1 {
+                                    for a in 0...compare1-1{
+                                        if Array(Jamo.getJamo(String(Array(whatYouAlreadyWrite)[aa-2])))[a] !=
+                                            Array(Jamo.getJamo(String(Array(whatYouHaveToWrite)[aa-2])))[a]{
+                                            miss += 1
+                                        }
+                                    }
+                                    miss += compare2 - compare1
+                                } else if compare2 == compare1 {
+                                    for a in 0...compare1-1{
+                                        if Array(Jamo.getJamo(String(Array(whatYouAlreadyWrite)[aa-2])))[a] !=
+                                            Array(Jamo.getJamo(String(Array(whatYouHaveToWrite)[aa-2])))[a]{
+                                            miss += 1
+                                        }
+                                    }
+                                } else if compare2 < compare1{
+                                    for a in 0...compare2-1{
+                                        if Array(Jamo.getJamo(String(Array(whatYouAlreadyWrite)[aa-2])))[a] !=
+                                            Array(Jamo.getJamo(String(Array(whatYouHaveToWrite)[aa-2])))[a]{
+                                            miss += 1
+                                        }
+                                    }
+                                    miss += compare2 - compare1
+                                }
+                            print(miss)
+                            aa += 1
+                        }
         return miss
-        
+    }
+    
+    func changeColor(_ viewLabel : UILabel, whatYouHaveToWrite : String, whatYouAlreadyWrite : String, textField : UITextField) {
+        let attr = NSMutableAttributedString(string: viewLabel.text!)
+                        for i in 0...Array(whatYouHaveToWrite).count-1{
+                            let completeTrigger = Array(whatYouAlreadyWrite).count
+                            if i <= Array(whatYouAlreadyWrite).count-1{
+                                if Array(String(whatYouAlreadyWrite))[i] == Array(whatYouHaveToWrite)[i] {
+                                    attr.addAttribute(.foregroundColor, value: UIColor.systemBlue, range: NSRange(location:i,length:1))
+                                    if i + 1 == completeTrigger{
+                                        textField.text?.append("*")
+                                        textField.text = textField.text?.trimmingCharacters(in: ["*"])
+                                    }
+                                }
+                                if i>0 {
+                                    if Array(String(whatYouAlreadyWrite))[i-1] != Array(whatYouHaveToWrite)[i-1]{
+                                        attr.addAttribute(.foregroundColor, value: UIColor.systemRed, range: NSRange(location:i-1,length:1))
+                                    }
+                                }
+                            } else{ break }
+                        }
+                        viewLabel.attributedText = attr
     }
     
 //    func calculateSpeed(_ time : String, answerArray : Array<Any>, arrayYouAreWriting : Array<Any>) -> Int {
 //        time
 //    }
+    
 }
--- Project_Tadak/MainViewController.swift
+++ Project_Tadak/MainViewController.swift
@@ -14,10 +14,8 @@ class MainViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var viewLabel: UILabel!
     @IBOutlet weak var inputTextField: UITextField!
     @IBOutlet weak var timeLabel: UILabel!
-    //이전문장, 다음문장 추가 : M
     @IBOutlet var afterLabel: UILabel!
     @IBOutlet var beforeLabel: UILabel!
-    //모서리 라운드 위한 View들 : M
     @IBOutlet var timeView: UIView!
     @IBOutlet var labelsView: UIView!
     var WrongCount = 0
@@ -43,15 +41,13 @@ class MainViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboard()
-        //View들 라운드 : M
         timeView.layer.cornerRadius = 14
         labelsView.layer.cornerRadius = 14
-        //시작전에 무엇을 쓸지 보여준다.
         beforeLabel.text = Text.init().textArray[0]
-        //timeLabel.text = String(second)
-        //input에 입력이 될시에 textFieldDidChange메소드 실행
         inputTextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
+        let calculate = Calculate()
     }
+    
     //input에 입력될 시 실행
     @objc func textFieldDidChange(textField: UITextField){
         if let input = textField.text{
@@ -60,125 +56,20 @@ class MainViewController: UIViewController, UITextFieldDelegate {
             //게임종료 조건 전까지 계속 실행
             if endStatus {
                 //문장 설정
-                if a == 0{
+                if a == 0 {
                     afterLabel.text = ""
                     viewLabel.text = Text.init().textArray[a]
                     beforeLabel.text = Text.init().textArray[a+1]
                 }
                 //비교를 위한 자모음 분해한 배열
-                let jamoYouHaveToWrite = Jamo.getJamo(viewLabel.text!)
-                let jamoYouAlreadyWrite = Jamo.getJamo(input)
-//                let luxuryJamoArrayYouAlreadyWrite : Array<Any>
-//                let luxuryJamoArrayYouHaveToWrite : Array<Any>
-                
-                //비교를 위한 글자 분해한 배열
                 let whatYouHaveToWrite = Text.init().textArray[a]
                 let whatYouAlreadyWrite = input
                 
-                if Array(input).count < aa-1 {
-                    if Array(input).count == 0{
-                    aa = 2
-                    }else{
-                    aa = Array(input).count + 1
-                    }
-                } else if Array(input).count == aa {
-//                    luxuryJamoArrayYouAlreadyWrite = Array(Jamo.getJamo(String(Array(input)[0...aa-2])))
-//                    luxuryJamoArrayYouHaveToWrite = Array(Jamo.getJamo(String(Array(input)[0...aa-2])))
-                        
-                        var compare1 = Array(Jamo.getJamo(String(Array(whatYouAlreadyWrite)[aa-2]))).count
-                        var compare2 = Array(Jamo.getJamo(String(Array(whatYouHaveToWrite)[aa-2]))).count
-                    
-                        if compare2 > compare1 {
-                            for a in 0...compare1-1{
-                                if Array(Jamo.getJamo(String(Array(whatYouAlreadyWrite)[aa-2])))[a] !=
-                                    Array(Jamo.getJamo(String(Array(whatYouHaveToWrite)[aa-2])))[a]{
-                                    miss += 1
-                                }
-                            }
-                            miss += compare2 - compare1
-                        } else if compare2 == compare1 {
-                            for a in 0...compare1-1{
-                                if Array(Jamo.getJamo(String(Array(whatYouAlreadyWrite)[aa-2])))[a] !=
-                                    Array(Jamo.getJamo(String(Array(whatYouHaveToWrite)[aa-2])))[a]{
-                                    miss += 1
-                                }
-                            }
-                        } else if compare2 < compare1{
-                            for a in 0...compare2-1{
-                                if Array(Jamo.getJamo(String(Array(whatYouAlreadyWrite)[aa-2])))[a] !=
-                                    Array(Jamo.getJamo(String(Array(whatYouHaveToWrite)[aa-2])))[a]{
-                                    miss += 1
-                                }
-                            }
-                            miss += compare2 - compare1
-                        }
-                    print(miss)
-                    aa += 1
-                }
-                
-                let attr = NSMutableAttributedString(string: viewLabel.text!)
-                
-//              여기 부분은 글자의 색을 바꾸고 자연스러운 글자흐름를 위한 부분
-                for i in 0...Array(whatYouHaveToWrite).count-1{
-                    completeTrigger = Array(whatYouAlreadyWrite).count
-                    if i <= Array(whatYouAlreadyWrite).count-1{
-                        if Array(String(whatYouAlreadyWrite))[i] == Array(whatYouHaveToWrite)[i] {
-                            attr.addAttribute(.foregroundColor, value: UIColor.systemBlue, range: NSRange(location:i,length:1))
-                            if i + 1 == completeTrigger{
-                                textField.text?.append("*")
-                                textField.text = textField.text?.trimmingCharacters(in: ["*"])
-                            }
-                        }
-                        
-                        if i>0 {
-                            if Array(String(whatYouAlreadyWrite))[i-1] != Array(whatYouHaveToWrite)[i-1]{
-                                attr.addAttribute(.foregroundColor, value: UIColor.systemRed, range: NSRange(location:i-1,length:1))
-                            }
-                        }
-                    } else{ break }
-                }
-                
-                //한글자 틀렸는지 인식하는 것
-                viewLabel.attributedText = attr
-                
-                
-//              오타개수 계산하기(실시간)
-                for i in 0...Array(whatYouHaveToWrite).count-1{
-                    completeTrigger2 = Array(whatYouAlreadyWrite).count
-                        if i + 1 == completeTrigger2 && i > 0 && completeTrigger2 - aa == 0{
-                            aa += 1
-                            var compare1 = Array(Jamo.getJamo(String(Array(whatYouAlreadyWrite)[i-1]))).count
-                            var compare2 = Array(Jamo.getJamo(String(Array(whatYouHaveToWrite)[i-1]))).count
-                            if compare2 > compare1 {
-                                for a in 0...compare1-1{
-                                    if Array(Jamo.getJamo(String(Array(whatYouAlreadyWrite)[i-1])))[a] !=
-                                        Array(Jamo.getJamo(String(Array(whatYouHaveToWrite)[i-1])))[a]{
-                                        miss += 1
-                                    }
-                                }
-                                miss += compare2 - compare1
-                            } else if compare2 == compare1 {
-                                for a in 0...compare1-1{
-                                    if Array(Jamo.getJamo(String(Array(whatYouAlreadyWrite)[i-1])))[a] !=
-                                        Array(Jamo.getJamo(String(Array(whatYouHaveToWrite)[i-1])))[a]{
-                                        miss += 1
-                                    }
-                                }
-                            } else if compare2 < compare1{
-                                for a in 0...compare2-1{
-                                    if Array(Jamo.getJamo(String(Array(whatYouAlreadyWrite)[i-1])))[a] !=
-                                        Array(Jamo.getJamo(String(Array(whatYouHaveToWrite)[i-1])))[a]{
-                                        miss += 1
-                                    }
-                                }
-                                miss += compare2 - compare1
-                            }
-                            print(miss)
-                        }
-                }
-                
+                calculate.countMiss(input, whatYouHaveToWrite: whatYouHaveToWrite, whatYouAlreadyWrite: whatYouAlreadyWrite)
+                calculate.changeColor(viewLabel, whatYouHaveToWrite: whatYouHaveToWrite, whatYouAlreadyWrite: whatYouAlreadyWrite, textField: textField)
+               
                 //타자를 치고있고 쳤는데 내용이 동일한 경우
-                if Array(jamoYouHaveToWrite) == Array(jamoYouAlreadyWrite){
+                if Array(whatYouHaveToWrite) == Array(whatYouAlreadyWrite){
                     a = a + 1
                     completeTrigger = 1
                     showArrays()
@@ -224,6 +115,28 @@ class MainViewController: UIViewController, UITextFieldDelegate {
         {
             afterLabel.text = Text.init().textArray[a-1]
             viewLabel.text = Text.init().textArray[a]
+            
+            self.beforeLabel.transform = CGAffineTransform(translationX: 0, y: 75)
+            beforeLabel.alpha = 0
+            UIView.animate(withDuration: 0.4) {
+                self.beforeLabel.alpha = 1;
+                self.beforeLabel.transform = CGAffineTransform(translationX: 0, y: 0)
+            }
+            
+            self.viewLabel.transform = CGAffineTransform(translationX: 0, y: 75)
+//            viewLabel.alpha = 0
+            UIView.animate(withDuration: 0.4) {
+//                self.viewLabel.alpha = 1;
+                self.viewLabel.transform = CGAffineTransform(translationX: 0, y: 0)
+            }
+            
+            self.afterLabel.transform = CGAffineTransform(translationX: 0, y: 75)
+            afterLabel.alpha = 1
+            UIView.animate(withDuration: 0.4) {
+                self.afterLabel.alpha = 0;
+                self.afterLabel.transform = CGAffineTransform(translationX: 0, y: 0)
+            }
+            
             beforeLabel.text = Text.init().textArray[a+1]
             inputTextField.text = ""
         }
@@ -250,21 +163,12 @@ class MainViewController: UIViewController, UITextFieldDelegate {
         //종료조건 : M
         endStatus = false
     }
-    
 
-    func arbitrua(firstInt : Int, SecondInt : Int, WW : Array<String>) throws {
-        guard Array(Jamo.getJamo(String(Array(WW)[firstInt-1]))).count > SecondInt else {
-            throw error.outOfRange
-        }
-    }
     
-    enum error : Error {
-        case outOfRange
-    }
 }
 
-extension Array {
-    subscript(safe index: Index) -> Element? {
-        return indices.contains(index) ? self[index] : nil
-    }
-}
+//extension Array {
+//    subscript(safe index: Index) -> Element? {
+//        return indices.contains(index) ? self[index] : nil
+//    }
+//}
