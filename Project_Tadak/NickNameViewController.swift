1b6ee36c56acdffd244c1af4cc04fb480cc92c04
shinhyogeun <tls1gy2rms3@naver.com>
2020-10-28 18:41:48 +0900

    make it possible to check if new nickname is unique or not

--- Project_Tadak/NickNameViewController.swift
+++ Project_Tadak/NickNameViewController.swift
@@ -24,13 +24,13 @@ class NickNameViewController: UIViewController {
         if let nick = nickNameLabel.text {
             
             let nickname = ["nickname":"\(nick)"]
-            let nickname2 = ["\(nick)":"\(Auth.auth().currentUser!.uid)"]
+            let nickname2 = ["nickname":"\(nick)"]
             
             let childUpdate = ["/users/\(Auth.auth().currentUser!.uid)/nickname" : nickname]
             let childUpdate2 = ["/nickname/\(nick)" : nickname2]
             self.ref.updateChildValues(childUpdate) { (error, dataSnapshot) in
                 if let error = error{
-                    print(error, "이미 다른 사람이 쓰고 있는 것 같습니다.")
+                    print(error.localizedDescription , "이미 다른 사람이 쓰고 있는 것 같습니다.")
                 } else{
                     self.ref.updateChildValues(childUpdate2)
                     DispatchQueue.main.async {
